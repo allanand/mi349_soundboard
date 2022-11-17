8fe15e9f864269374bdde85fbd967362b82cdb74
allanand <112962855+allanand@users.noreply.github.com>
2022-11-16 20:55:10 -0500

    lost track 2

--- soundboard.js
+++
@@ -1,6 +0,0 @@
-var work = document.getElementById("play");
-    function playMusic() {
-        let audio = new Audio("fart.mp3");
-        audio.play()
-}
-play.addEventListener("click", playMusic);
