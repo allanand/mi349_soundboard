877c0ccfe993c5088e2af4cbbe339312d9f33e1e
allanand <112962855+allanand@users.noreply.github.com>
2022-11-16 20:30:08 -0500

    test 36

--- soundboard.js
+++ soundboard.js
@@ -1,6 +1,6 @@
 let work = document.getElementById("work");
     function music() {
-        let audio = Audio("fart.mp3");
+        let audio = new Audio("fart.mp3");
         audio.play()
 }
 play.addEventListener("click", music);
